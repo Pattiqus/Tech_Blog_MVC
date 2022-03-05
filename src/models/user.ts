ee9fb33730b1bd41bb23f1dd69b731bdfe52014c
unknown <patticus.tv@gmail.com>
2022-03-05 20:49:15 +1100

    TS and bcrypt error in user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,10 @@
 import { Model, DataTypes } from 'sequelize';
+import { defaultValueSchemable } from 'sequelize/types/utils';
 // const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt'
+// const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-
+const userSchema = new 
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
